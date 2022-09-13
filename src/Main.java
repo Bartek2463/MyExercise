15476a058ba32ed8a81d6121513649b311ff5761
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-09-13 10:16:06 +0200

    Task 1 calculate the sum beetween numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Sum(10, -3));
+    }
+    public static int Sum( int a, int b){
+        int sum = 0;
+        for (int i = Math.min(a,b); i <=Math.max(a,b) ; i++) {
+            sum+=i;
+        }
+        return sum;
     }
 }
