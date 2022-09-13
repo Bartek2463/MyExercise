72e3a34000b3614011f72dec375b4f3debae6fe5
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-09-13 13:21:52 +0200

    Stream  Solution

--- src/SmashWords.java
+++ src/SmashWords.java
@@ -1,9 +1,12 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class SmashWords {
 
     public static void main(String[] args) {
         System.out.println(smash("ala", "kota"));
     }
     public static String smash(String... words){
-        return String.join(" ",words);
+     return Stream.of(words).collect(Collectors.joining(" "));
     }
 }
