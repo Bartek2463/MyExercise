643256768eaabadcc0bc23c4da5b295abdf28b6b
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-09-15 22:42:24 +0200

    Revert "Other Solution"
    This reverts commit 6d59251d

---
+++ src/Isogram.java
@@ -0,0 +1,11 @@
+import java.util.stream.IntStream;
+
+public class Isogram {
+    public static void main(String[] args) {
+        System.out.println(isoGram("moOse"));
+    }
+    public static  boolean isoGram(String str){
+       return str.length()== str.toLowerCase().chars().distinct().count();
+
+    }
+}
