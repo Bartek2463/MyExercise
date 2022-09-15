2840a19a0fa3b4616e9109f4515a7eb00a0c571d
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-09-15 21:18:10 +0200

    Main Solution

--- src/FilterList.java
+++ src/FilterList.java
@@ -1,3 +1,17 @@
-public class FilterList {
+import java.util.List;
+import java.util.Objects;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
 
+public class FilterList {
+    public static void main(String[] args) {
+        System.out.println(list(List.of("a", "b", "c", 1, 2, 3, 4)));
+    }
+    public static List<Object> list (List<Object> list){
+       return list.stream().filter(e->e instanceof Integer)
+                .collect(Collectors.toList());
+
+
+
+    }
 }
