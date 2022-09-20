75ee47db53604e426a5f0202954a2110a630bd66
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-09-20 20:11:55 +0200

    Main Solution

---
+++ src/EvenOrOdd.java
@@ -0,0 +1,8 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        System.out.println(evenOrOdd(2));
+    }
+    public static  String evenOrOdd(int number){
+        return number%2==0 ?"Even":"Odd";
+    }
+}
