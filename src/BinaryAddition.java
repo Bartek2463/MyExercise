26323478b3af03eb810be72e36b51c734e99dc39
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-09-25 23:01:27 +0200

    Main solution simple Task

--- src/BinaryAddition.java
+++ src/BinaryAddition.java
@@ -1,9 +1,10 @@
 public class BinaryAddition {
 
     public static void main(String[] args) {
-
+        System.out.println(binaryAddition(4, 6));
     }
      public static String binaryAddition(int a, int b){
-        return null;
+
+           return  Integer.toBinaryString(a+b);
      }
 }
