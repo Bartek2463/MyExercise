c6b8abd2035c085e1679e907d9442cdb4c5be32f
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-09-29 15:27:01 +0200

    Other Solution by someone using Intstream

--- src/Positive.java
+++ src/Positive.java
@@ -9,6 +9,7 @@ Note: if there is nothing to sum, the sum is default to 0.*
 
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Positive {
 
@@ -17,7 +18,7 @@ public class Positive {
     }
 
     public static int sum(int[]arr) {
-         return Arrays.stream(arr).filter(v ->v>0).sum();
+       return IntStream.of(arr).filter(i->i >0).sum();
     }
 }
 
