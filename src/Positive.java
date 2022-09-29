6094da02c5ae227f0b0aa12731ccd551636ec57f
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-09-29 15:25:31 +0200

    Other Solution by someone

--- src/Positive.java
+++ src/Positive.java
@@ -8,20 +8,16 @@ Note: if there is nothing to sum, the sum is default to 0.*
  */
 
 
+import java.util.Arrays;
+
 public class Positive {
 
     public static void main(String[] args) {
         System.out.println(sum(new int[]{1, 2, 3, 4, 5,-5}));
     }
 
-    public static int sum(int[]arr){
-        int sum = 0;
-        for (int i = 0; i <arr.length ; i++) {
-            if(arr[i]>0){
-                sum+=arr[i];
-            }
-        }
-        return sum;
+    public static int sum(int[]arr) {
+         return Arrays.stream(arr).filter(v ->v>0).sum();
     }
 }
 
